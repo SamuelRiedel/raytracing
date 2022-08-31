8e6c84d7ad9229e3a684e59757bed8ee5e966b1c
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-08-31 14:48:13 +0200

    Scale rays for objects without reflection

--- raytracer.cpp
+++ raytracer.cpp
@@ -39,6 +39,8 @@
 #define WIDTH (1280)
 #define HEIGHT (720)
 
+#define PRECISION (2048.0)
+
 #define VEC3_xyz(X, Y, Z) ((Vec3) { .x = X, .y = Y, .z = Z })
 #define VEC3_x(X) VEC3_xyz(X, X, X)
 #define VEC3 VEC3_x(0)
@@ -79,7 +81,7 @@ Vec3 vec_normalize(Vec3 v) {
     float invNor = 1 / sqrt(nor2);
     v = vec_scale(v, invNor);
   }
-  return v;
+  return vec_scale(v, PRECISION);
 }
 
 void vec_print(Vec3 v) { printf("%f, %f, %f\n", v.x, v.y, v.z); }
@@ -95,7 +97,7 @@ typedef struct Sphere {
 bool intersect(const Sphere sphere, const Vec3 rayorig, const Vec3 raydir,
                float *t0, float *t1) {
   Vec3 l = vec_subtract(sphere.center, rayorig);
-  float tca = vec_dot(l, raydir);
+  float tca = vec_dot(l, raydir) / PRECISION;
   if (tca < 0)
     return false;
   float d2 = vec_length2(l) - tca * tca;
@@ -104,6 +106,8 @@ bool intersect(const Sphere sphere, const Vec3 rayorig, const Vec3 raydir,
   float thc = sqrt(sphere.radius2 - d2);
   *t0 = tca - thc;
   *t1 = tca + thc;
+  // printf("l %3.3f, tca %3.3f, d2 %3.3f, thc %3.3f, t0 %3.3f t1 %3.3f\n", l.x,
+  // tca, d2, thc, *t0, *t1);
 
   return true;
 }
@@ -152,13 +156,15 @@ Vec3 trace(const Vec3 rayorig, const Vec3 raydir, const Sphere *spheres,
   // if there's no intersection return black or background color
   if (!sphere)
     return VEC3_x(240);
-  Vec3 surfaceColor =
-      VEC3; // color of the ray/surfaceof the object intersected by the ray
+  // color of the ray/surfaceof the object intersected by the ray
+  Vec3 surfaceColor = VEC3;
+  // point of intersection
   Vec3 phit =
-      vec_add(rayorig, vec_scale(raydir, tnear)); // point of intersection
-  Vec3 nhit =
-      vec_subtract(phit, sphere->center); // normal at the intersection point
-  nhit = vec_normalize(nhit);             // normalize normal direction
+      vec_add(rayorig, vec_scale(vec_scale(raydir, tnear), 1.0 / PRECISION));
+  // normal at the intersection point
+  Vec3 nhit = vec_subtract(phit, sphere->center);
+  // normalize normal direction
+  nhit = vec_normalize(nhit);
   // If the normal and the view direction are not opposite to each other
   // reverse the normal direction. That also means we are inside the sphere so
   // set
@@ -224,7 +230,7 @@ Vec3 trace(const Vec3 rayorig, const Vec3 raydir, const Sphere *spheres,
         }
         float dot = vec_dot(nhit, lightDirection);
         if (dot > 0) {
-          transmission *= dot;
+          transmission *= dot / PRECISION / PRECISION;
           Vec3 tmp = vec_scale(sphere->surfaceColor, transmission);
           tmp = vec_multiply(tmp, spheres[i].emissionColor);
           tmp = vec_scale(tmp, 0.00390625); // Divide by 256
@@ -296,7 +302,7 @@ int main(int argc, char **argv) {
                           .surfaceColor = VEC3_xyz(249, 199, 79),
                           .emissionColor = VEC3,
                           .transparency = 0,
-                          .reflection = 1 };
+                          .reflection = 0 };
   spheres[3] = (Sphere) { .center = VEC3_xyz(5.0, 0, -25),
                           .radius = 3,
                           .radius2 = 3 * 3,
@@ -326,6 +332,11 @@ int main(int argc, char **argv) {
                           .emissionColor = VEC3_x(255),
                           .transparency = 0,
                           .reflection = 0 };
+  for (int i = 0; i < num_spheres; ++i) {
+    spheres[i].center = vec_scale(spheres[i].center, PRECISION);
+    spheres[i].radius = spheres[i].radius * PRECISION;
+    spheres[i].radius2 = spheres[i].radius2 * PRECISION * PRECISION;
+  }
   render(spheres, num_spheres);
 
   return 0;
